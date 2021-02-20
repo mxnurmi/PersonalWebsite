0a3519c9d304755206fc41ffe25be21f78a5e406
Miska <miska.nurmi@helsinki.fi>
2021-02-20 18:47:07 +0200

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
 import logo from './logo.svg'
 //<img src={logo} className="App-logo" alt="logo" />
 //import Container from '@material-ui/core/Container'
-import { ThemeProvider, AppBar, Toolbar } from '@material-ui/core/'
+import { ThemeProvider, AppBar, Toolbar, Button, Box, Grid, Paper } from '@material-ui/core/'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+
 //import Typography from '@material-ui/core/Typography'
 import React from 'react'
 import theme from './theme'
 
-const Sitelink = () => {
+//we need to load the portfolio images and the text
 
+const Linkbar = () => {
+  const buttonStyle = {
+    background: 'secondary',
+    borderRadius: 3,
+    border: 0,
+    color: 'Black',
+    fontSize: 20,
+    fontWeight: 600,
+    height: 48,
+    padding: '0 30px',
+  }
+
+  return(
+    <Box m={2}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Box ml={0} p={1}>
+            <img src={logo} alt='logo' width='35' height='35'/>
+          </Box>
+          <Box ml='auto' mr={0}>
+            <LinkedInIcon />
+          </Box>
+          <Box ml={0} mr={0} p={2} >
+            <Button style={buttonStyle} size='small' variant='contained' color='secondary'>Test</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+const Portfolio = () => {
+
+  const paperStyle = {
+    padding: theme.spacing(4),
+    margin: 'auto',
+    maxWidth: 500,
+  }
+
+  //a portfolio component should have content image fields and then something to decide the order
+
+  return(
+    <Grid container spacing={2}>
+      <Grid item xs>
+        <Paper xs={10} style={paperStyle}>xs</Paper>
+      </Grid>
+      <Grid item xs>
+        <Paper style={paperStyle}>xs</Paper>
+      </Grid>
+    </Grid>
+  )
 }
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <AppBar position="static">
-        <Toolbar><img src={logo} alt="logo" width="100" height="100" /></Toolbar>
-      </AppBar>
+      <Linkbar />
+      <Portfolio />
     </ThemeProvider>
   )
 }
-
-{/* <Container color="primary">
-<div>
-  <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
-  welcome
-</div>
-</Container> */}
 
 export default App
--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,12 @@
 import { createMuiTheme }  from '@material-ui/core/styles'
 
 const theme = createMuiTheme({
-    palette: {
-        primary: { 500: '#17096c' },
-    },
-    secondary: {
-        primary: { 500: '#5d6c09' },
-    },
+  palette: {
+    primary: { 500: '#17096c' },
+    secondary: { main: '#8cbf32' },
+  },
+  margin: 10,
+  //spacing: [0, 4, 8, 16, 32, 64],
 })
 
 export default theme
