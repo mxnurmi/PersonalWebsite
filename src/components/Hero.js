25fc4b4600e5384f60ad0421974da5ad3dec551e
Miska <miska.nurmi@helsinki.fi>
2021-02-22 09:56:27 +0200

    updated layout and created module for scatterplot

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,17 +12,17 @@ const Hero = ({ title, subTitle, text }) => (
         <Col md={8}>
           {title && <h1 className=" display-2 font-weight-bolder">{title}</h1>}
           {subTitle && <h3 className="display-4 font-weight-light">{subTitle}</h3>}
+          {text && <h3 className="lead font-weight-light">{text}</h3>}
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center py-4">
+        <Col sm={8} md={8} lg={8}>
         </Col>
       </Row>
       <Row className="justify-content-center py-1">
         <Col md={8}>
           <Button variant="light" href="/portfolio">Portfolio</Button>
           <Button variant="light" href="/projects">Projects</Button>
-        </Col>
-      </Row>
-      <Row className="justify-content-md-center py-4">
-        <Col sm={8} md={8} lg={8}>
-          {text && <h3 className="text-muted">{text}</h3>}
         </Col>
       </Row>
     </Container>
